202b3e941288ea70bbdb7d2deca3c995f5b92794
Siri <8502281+siri404@users.noreply.github.com>
2025-07-08 09:09:57 +0100

    Create cnames_active.js

---
+++ cnames_active.js
@@ -0,0 +1,8 @@
+var cnames_active = {
+  "siri404": "siri404.github.io"
+  /*
+   * please don't add your subdomain records down here!
+   * insert them in alphabetical order to help reduce merge conflicts.
+   * <3
+   */
+}
